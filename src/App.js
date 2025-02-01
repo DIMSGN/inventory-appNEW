05921a8e9f014e61a38fe801a022c87b1c025f43
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-02-01 23:47:55 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Greeting() {
+
+  return (
+    <>
+      <h1>Hello World with React</h1>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adipiscing elit.  Donec auctor, nunc nec  
+
+
+      </p>
+   </>
+  );
+ }
+
+export default Greeting;
